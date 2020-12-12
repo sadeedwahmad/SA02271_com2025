0e2147ca70be212190cff3a49f12d0aee1c698e9
Sadeed Ahmad <sadeed987@hotmail.co.uk>
2020-12-12 13:39:22 +0000

    Improved Views

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,7 @@ class Profile < ApplicationRecord
   has_attached_file :picture
   do_not_validate_attachment_file_type :picture
 
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :user_id, presence: true, uniqueness: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks  ,dependent: :destroy
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
+  validates :description,presence: true
+
+
 end
